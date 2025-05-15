f69f1ca92e0802d5f73d8ed200fc50c7b4a19e44
jmathtech <jamil.matheny@gmail.com>
2025-05-15 11:52:40 -0400

    Adding text to footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -345,7 +345,7 @@ export default function Home() {
           <h2 className="text-2xl font-bold mb-4">Contact Me</h2>
           <div className=" bg-black border border-gray-700 text-white p-12 rounded-lg transition duration-300 ease-in-out transform hover:scale-103"
           >
-            <p className="mb-4">Have a project in mind? Reach out to me at:</p>
+            <p className="mb-4">Do you have a project in mind? Reach out to me at:</p>
             <p className="mb-4">
               <Link
                 href="mailto:jamil.matheny@majestikmagik.com"
