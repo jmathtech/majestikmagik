bfd6503b0c40d43d39b691308442c90e8729a784
jmathtech <jamil.matheny@gmail.com>
2025-05-20 13:00:58 -0400

    Updating hero section for visual appealing look

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,8 @@ export const metadata: Metadata = {
       { rel: 'icon', type: 'image/x-icon', url: '/favicon.ico', sizes: 'any' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon-16x16.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '48x48', url: '/favicon-48x48.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', url: '/favicon-192x192.png' },
       { rel: 'icon', type: 'image/png', sizes: 'any', url: '/media-icon.png' },
     ],
     apple: '/apple-touch-icon.png',
